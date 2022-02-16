9651b51f133dc845671c5260378945e3e1fc0f29
Hexagon666666 <99750559+Hexagon666666@users.noreply.github.com>
2022-02-16 18:04:51 +0100

    Update umkehrung.java
    Kommentare hinzugefügt

--- umkehrung.java
+++ umkehrung.java
@@ -4,7 +4,9 @@ System.out.print("Textlänge: ");
 System.out.print(laenge);
 System.out.println(" Zeichen");
 System.out.println("-------------------------------");
+// Die Variable i gibt die Position des aktuellen Buchstabens an, dabei wird von hinten begonnen
 int i = laenge - 1;
+// Solange i nicht am Anfang des Wortes angekommen ist, wird der Buchstabe an der Stelle i ausgegeben und i dann um 1 verringert.
 while(i >= 0) {
    char c = text.charAt(i);
    System.out.print(c);
