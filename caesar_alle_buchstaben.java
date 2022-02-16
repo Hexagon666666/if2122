ca686300947e150aefeceed4e21e23b9f12f26fd
Hexagon666666 <99750559+Hexagon666666@users.noreply.github.com>
2022-02-16 09:50:22 +0100

    Create caesar_alle_buchstaben.java

---
+++ caesar_alle_buchstaben.java
@@ -0,0 +1,51 @@
+String text = Input.readString("Gib einen Text ein");
+int laenge = text.length();
+int schluessel = Input.readInt("Schlüssel eingeben");
+System.out.print("Textlänge: ");
+System.out.print(laenge);
+System.out.println(" Zeichen");
+System.out.println("-------------------------------");
+while(schluessel > 26) {
+   schluessel = schluessel - 26;
+}
+int i = 0;
+int j = 0;
+int grossbuchstabe = 0;
+while(i < laenge) {
+   char c = text.charAt(i);
+   j = (int) c;
+   if(j >= 65 && j <= 90) {
+      grossbuchstabe = 1;
+   }else{
+      grossbuchstabe = 0;
+   }
+   j = j + schluessel;
+   if(grossbuchstabe == 1) {
+      if(j <= 90) {
+         c = (char) j;
+         print(c);
+      }else{
+         j = j - 26;
+         c = (char) j;
+         print(c);
+      }
+   }else{
+      if(j <= 122) {
+         if(j >= 97) {
+            c = (char) j;
+            print(c);
+         }else{
+         j = j + 26;
+         c = (char) j;
+         print(c);
+         }
+         
+      }else{
+         j = j - 26;
+         c = (char) j;
+         print(c);
+      }
+
+   }
+   i = i + 1;
+}
